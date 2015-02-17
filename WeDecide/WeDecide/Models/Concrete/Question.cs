633b2a1c83db0a8754a89bb064d44d56f0380e5b
Stephen Fox <stevemasta34@gmail.com>
2015-02-17 16:47:26 -0700

    Here's the thing

--- WeDecide/WeDecide/Models/Concrete/Question.cs
+++ WeDecide/WeDecide/Models/Concrete/Question.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace WeDecide.Models.Concrete
 {
-    public class Question
+    public abstract class Question
     {
+        public string Text { get; set; }
+        public List<Response> Responses { get; set; }
+        public Scope QuestionScope { get; set; }
+        public bool? FreeResponseEnabled { get; set; }
+        public enum Scope
+        {
+            Friends,
+            Local,
+            World
+        }
     }
 }
---
+++ WeDecide/WeDecide/Models/Concrete/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeDecide.Models.Concrete
+{
+    public class Response
+    {
+        public Question Owner { get; set; }
+        public string Text { get; set; }
+    }
+}
