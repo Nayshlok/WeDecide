18d50f25d84bee0e789fc75253be63f600922cec
Jadon Leininger <jleininger@student.neumont.edu>
2015-02-16 17:28:00 -0700

    Added in basic controller classes

---
+++ WeDecide/WeDecide/Controllers/QuestionAnalyticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeDecide.Controllers
+{
+    public class QuestionAnalyticsController : Controller
+    {
+        // GET: QuestionAnalytics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
