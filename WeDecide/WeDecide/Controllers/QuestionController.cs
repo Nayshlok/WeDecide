012c9f1f938a014093021148ff57054cb4e51905
Jacob Blodgett <jblodgett@student.neumont.edu>
2015-02-18 15:35:19 -0700

    my thing

--- WeDecide/WeDecide/Controllers/QuestionController.cs
+++ WeDecide/WeDecide/Controllers/QuestionController.cs
@@ -28,8 +28,8 @@ namespace WeDecide.Controllers
             }
             else
             {
+                return View();
             }
-            return View();
         }
         }
         
