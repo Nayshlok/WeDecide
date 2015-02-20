806fa3d2fa364fb7b8b2517015014d70488acdd1
Jacob Blodgett <jblodgett@student.neumont.edu>
2015-02-20 15:52:22 -0700

    Something in HomeController

--- WeDecide/WeDecide/Controllers/HomeController.cs
+++ WeDecide/WeDecide/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeDecide.ViewModels;
 
 namespace WeDecide.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet]
         public ActionResult Index()
         {
             //If the user is logged in, take him to the home page with feed.  
