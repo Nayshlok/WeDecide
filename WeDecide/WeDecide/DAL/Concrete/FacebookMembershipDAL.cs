21cb2b50ef5208af410ed9b797906f1162b7863f
biced1 <iamkevindustin@gmail.com>
2015-02-26 16:17:37 -0700

    to annoy fox

--- WeDecide/WeDecide/DAL/Concrete/FacebookMembershipDAL.cs
+++ WeDecide/WeDecide/DAL/Concrete/FacebookMembershipDAL.cs
@@ -8,5 +8,24 @@ namespace WeDecide.DAL.Concrete
 {
     public class FacebookMembershipDAL : IMembershipDAL
     {
+        public void AddUser(string Name, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Concrete.User GetUser(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Models.Concrete.User> Search(string Search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Models.Concrete.User> GetFriends(string Id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- WeDecide/WeDecide/Models/Concrete/Question.Designer.cs
+++ WeDecide/WeDecide/Models/Concrete/Question.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Stephen\Source\Repos\WeDecide\WeDecide\WeDecide\Models\Concrete\Question.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\Kevin Dustin\Documents\GitHub\WeDecide\WeDecide\WeDecide\WeDecide\Models\Concrete\Question.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
