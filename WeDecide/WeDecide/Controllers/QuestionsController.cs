b5a80a2c8358956ffbaeba38cfa1a8e01a866fc5
Stephen Fox <stevemasta34@gmail.com>
2015-03-03 23:49:49 -0700

    Question WebAPI controller now works as expected. NOTE: you must request JSON, otherwise non-IE-browsers will request XML (for whatever reason)

--- WeDecide/WeDecide/Controllers/QuestionsController.cs
+++ WeDecide/WeDecide/Controllers/QuestionsController.cs
@@ -26,16 +26,33 @@ namespace WeDecide.Controllers
         }
 
         // GET: api/Questions
-        public IEnumerable<Question> GetQuestion()
+        public IEnumerable<QuestionDTO> GetQuestion()
         {
-            return _questionLayer.GetAll(x => true);
+            return _questionLayer.GetAll(x => true)
+                .Select(q => new QuestionDTO() {
+                    Id  = q.Id,
+                    QuestionText = q.Text,
+                    IsActive = q.IsActive,
+                    EndTime = q.EndDate
+                });
         }
 
         // GET: api/Questions/5
-        [ResponseType(typeof(Question))]
+        [ResponseType(typeof(QuestionDTO))]
         public async Task<IHttpActionResult> GetQuestion(int id)
         {
-            var question = await Task.Factory.StartNew<Question>(() => _questionLayer.Get(id));
+            var question = await Task.Factory
+                .StartNew<QuestionDTO>(() => {
+                    var innerQ = _questionLayer.Get(id);
+
+                    return new QuestionDTO
+                    {
+                        QuestionText = innerQ.Text,
+                        Id = innerQ.Id,
+                        IsActive = innerQ.IsActive,
+                        EndTime = innerQ.EndDate
+                    };
+                });
 
             if (question == null)
                 return NotFound();
@@ -60,5 +77,13 @@ namespace WeDecide.Controllers
         public void Delete(int id)
         {
         }
+    }
+
+    public class QuestionDTO
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public string QuestionText { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
