67e43793d28570b8b530182289288c0f35aa18da
Nayshlok <nineborland@gmail.com>
2015-03-04 17:50:24 -0700

    Added notifications to model and database.

--- WeDecide/WeDecide/Models/Concrete/Question2.Context.cs
+++ WeDecide/WeDecide/Models/Concrete/Question2.Context.cs
@@ -28,5 +28,6 @@ namespace WeDecide.Models.Concrete
         public virtual DbSet<Question> Questions { get; set; }
         public virtual DbSet<Response> Responses { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Notification> Notifications { get; set; }
     }
 }
