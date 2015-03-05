2c98507bf06485e3fdca1ac4b29d6e56db4256a2
Nayshlok <nineborland@gmail.com>
2015-03-05 16:08:49 -0700

    It had excluded files

---
+++ WeDecide/WeDecide/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeDecide.DAL.Abstract;
+using WeDecide.Models.Concrete;
+using Microsoft.AspNet.Identity.Owin;
+using WeDecide.ViewModels;
+
+namespace WeDecide.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class AdminController : Controller
+    {
+        private IMembershipDAL Mdal;
+        private ApplicationDbContext iddb;
+
+        public AdminController(IMembershipDAL dal, ApplicationDbContext iddb)
+        {
+            Mdal = dal;
+            this.iddb = iddb;
+        }
+
+        // GET: Admin
+        [HttpGet]
+        public ActionResult UserAdmin()
+        {
+            AdminViewModel model = new AdminViewModel(HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>()) { Users = Mdal.GetUsers(), IdUsers = iddb.Users };
+            return View("~/Views/Account/AdminUsers.cshtml", model);
+        }
+
+        public ActionResult RemoveUser()
+        {
+            string id = Request.Form["userId"];
+            //Remove User
+            return new RedirectResult("UserAdmin");
+        }
+
+        public ActionResult ChangeRole()
+        {
+            string id = Request.Form["userId"];
+            UserRoles role;
+            Enum.TryParse<UserRoles>(Request.Form["Role"], out role);
+            Mdal.SetRole(id, role);
+            return new RedirectResult("UserAdmin");
+        }
+
+    }
+}
---
+++ WeDecide/WeDecide/ViewModels/AdminViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using WeDecide.Controllers;
+using WeDecide.Models.Concrete;
+
+namespace WeDecide.ViewModels
+{
+    public class AdminViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<ApplicationUser> IdUsers { get; set; }
+        public IEnumerable<Question> Questions { get; set; }
+        public IEnumerable<Response> Responses { get; set; }
+        public SelectList RoleList { get; set; }
+        [Inject]
+        public ApplicationUserManager userManager { get; set; }
+
+        public AdminViewModel(ApplicationUserManager manager)
+        {
+            userManager = manager;
+            RoleList = CreateRoleList();
+        }
+
+        public SelectList CreateRoleList()
+        {
+            var roles = Enum.GetValues(typeof(UserRoles));
+            return new SelectList(roles);
+        }
+    }
+}
