5000d913578b3d1c8fd0178b446bba07bae8e02e
stevemasta34 <stevemasta34@gmail.com>
2015-03-13 12:33:23 -0600

    Initial checking of a checkbox triggers a good pull from the WebAPI, but thereafter the view doesn't update. This is either ng-repeat or Javascript Array type issues of which I was previously unaware.

--- WeDecide/WeDecide/Scripts/app/GlobalFeed.js
+++ WeDecide/WeDecide/Scripts/app/GlobalFeed.js
@@ -39,7 +39,7 @@
     }]);
 
     feedApp.controller('HomePageCtrl', ['$scope', '$http', function ($scope, $http) {
-        
+
         var self = this;
         self.allQuestionURL = '/api/questions/GetFilteredQuestions/';
         self.singleResponseURL = 'api/questions/GetResponse/';
@@ -71,42 +71,54 @@
                 success(function (data, headers, status, config) {
                     console.log("Successfully fetched {0}, with length = {1}".format(data, data.length));
                     poolInQuestion = data; // no need for pushing, just copy the Array
+
+                    poolInQuestion.forEach(function (element) {
+                        $scope.questions.push(element);
+                    });
                 }).
                 error(function (data, headers, status, config) {
                     printFailures(data); // defined in functions.js
                 });
-            // Update the feed with the new concerns
-            $scope.questions = poolInQuestion;
+            // Update the feed with the new concerns           
+            //$scope.questions = poolInQuestion;
         };
 
         // TODO: set the interval function and click event on 'checkbox.checkEvent'
         // TODO: decouple for the ng-click and pass the id into each call
-        function doQuestionFlow() {
+        $scope.doQuestionFlow = function(filter) {
             console.log("Down in the questionFlow");
             // while the box is checked, update the pools at an interval
-            if (friendsCheckBox.checked) {
-                setTimeout(function () {
-                    // refresh the friends pool
-                    ajaxQuestionLoader("friends", friendsQuestionPool);
-                }, 3000);
-            }
 
-            if (localCheckBox.checked) {
-                setTimeout(function () {
-                    // refresh the local pool
-                    ajaxQuestionLoader("local", localQuestionPool);
-                }, 5000);
-            }
-
-            if (globalCheckBox.checked) {
-                setTimeout(function () {
-                    // refresh the global pool
-                    ajaxQuestionLoader("global", globalQuestionPool);
-                }, 7000);
-            }
+            if (filter.toString().toLowerCase() === "friends")
+                if (friendsCheckBox.checked) {
+                    console.log("Setting friends")
+                    setTimeout(function () {
+                        // refresh the friends pool
+                        ajaxQuestionLoader("friends", friendsQuestionPool);
+                    }, 3000);
+                }
+                else
+                {
+                    console.log("Emptying friend questions");
+                    $scope.questions = [];
+                }
+            else if (filter.toString().toLowerCase() === "global")
+                if (globalCheckBox.checked) {
+                    console.log("Setting global");
+                    setTimeout(function () {
+                        // refresh the global pool
+                        ajaxQuestionLoader("global", globalQuestionPool);
+                    }, 7000);
+                }
+                else
+                {
+                    console.log("Emptying global questions");
+                    $scope.questions = [];
+                }
         }
 
-        setInterval(doQuestionFlow, 3000);
+        $scope.doQuestionFlow("global");
+        //setInterval(doQuestionFlow, 6000);
     }]);
 
     feedApp.controller('ProfileCtrl', ['$scope', '$http', function ($scope, $http) {
